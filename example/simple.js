a10b0fa107f83e1409a9f726219bc814acb1b787
sanemat <o.gata.ken@gmail.com>
2014-06-11 09:52:03 +0900

    Exec `yo node-gulp`

---
+++ example/simple.js
@@ -0,0 +1,25 @@
+/*
+ * google-transliterate
+ * https://github.com/sanemat/google-transliterate
+ *
+ * Copyright (c) 2014 sanemat
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// Following the 'Node.js require(s) best practices' by
+// http://www.mircozeiss.com/node-js-require-s-best-practices/
+
+// // Nodejs libs
+// var fs = require('fs'),
+//
+// // External libs
+// debug = require('debug'),
+//
+// // Internal libs
+// data = require('./data.js');
+
+var googleTransliterate = require('../');
+
+googleTransliterate.awesome(); // "awesome"
---
+++ test/google-transliterate_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var googleTransliterate = require('../lib/google-transliterate.js');
+var assert = require('should');
+
+describe('googleTransliterate', function () {
+
+  it('should be awesome', function () {
+    googleTransliterate.awesome().should.equal('awesome');
+  });
+
+});
