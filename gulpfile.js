c36587907c2c9985613685b77c95e7846c9da8df
sanemat <o.gata.ken@gmail.com>
2014-06-13 01:11:38 +0900

    Use reporter spec

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('istanbul', function (cb) {
     .on('finish', function () {
       gulp.src(paths.tests)
         .pipe(plugins.plumber())
-        .pipe(plugins.mocha())
+        .pipe(plugins.mocha({ reporter: 'spec' }))
         .pipe(plugins.istanbul.writeReports()) // Creating the reports after tests runned
         .on('finish', function() {
           process.chdir(__dirname);
