343a14d22df8a8eac1077c17a673892a0fd4c9fd
sanemat <o.gata.ken@gmail.com>
2014-06-13 19:42:14 +0900

    Add multi literate

--- test/google-transliterate_test.js
+++ test/google-transliterate_test.js
@@ -6,7 +6,17 @@ var assert = require('power-assert');
 describe('googleTransliterate', function () {
   it('should transliterate', function (done) {
     googleTransliterate.transliterate('かんだ', 'ja-Hira', 'ja', function(err, transliteration){
-      assert.equal(transliteration, '神田');
+      assert.deepEqual(transliteration, [ [ 'かんだ', [ '神田', '噛んだ', '勘だ', 'かんだ', '鑑だ' ] ] ]);
+      done();
+    });
+  });
+
+  it('should transliterate multi literal', function (done) {
+    googleTransliterate.transliterate('おあややおやにおあやまり', 'ja-Hira', 'ja', function(err, transliteration){
+      assert.deepEqual(transliteration,
+        [ [ 'おあやや', [ 'お文や', 'おあやや', 'お彩や', 'お綾や', 'おアヤや' ] ],
+        [ 'おやに', [ '親に', 'おやに', 'オヤに', 'お屋に', 'お矢に' ] ],
+        [ 'おあやまり', [ 'お誤り', 'お謝り', 'おあやまり', 'オアヤマリ', 'ｵｱﾔﾏﾘ' ] ] ]);
       done();
     });
   });
